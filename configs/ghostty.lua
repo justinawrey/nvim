6805b6504e0b06fae074e319b2b0bd914fe9c910
Justin Awrey <awreyjustin@gmail.com>
2026-01-12 17:09:48 -0800

    oops

---
+++ configs/ghostty.lua
@@ -0,0 +1,12 @@
+-- note this only works if cwd is the root wherever
+-- this repo is cloned... kinda wack but its mine so w/e.
+-- just use nvime alias
+require('init')
+
+-- Open default tabs the way I like it
+vim.cmd('tabe')
+vim.cmd('tcd ~')
+vim.cmd('e ~/.config/daily/daily.md')
+vim.cmd('tabn 1')
+vim.cmd('Tname 1 ghostty')
+vim.cmd('Tname 2 notes')
